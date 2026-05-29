c40ff2562b89943bc2e017ff164c12c65f8d00bf
qingchen3 <qchen.cs3@gmail.com>
2026-05-29 11:16:06 +0800

    add dynamic connectivity interface and STree wrapper

--- extension/algo/src/include/common/dynamic_connectivity_index.h
+++ extension/algo/src/include/common/dynamic_connectivity_index.h
@@ -1,21 +1,22 @@
 #pragma once
 
-#include <cstdin>
+#include <cstdint>
 #include <string>
 
-namespace kuzu{
+namespace kuzu {
 namespace algo_extension {
+
 class DynamicConnectivityIndex {
 public:
     using node_key_t = int64_t;
 
     virtual ~DynamicConnectivityIndex() = default;
 
-    virtual void ~insertEdge(node_key_t u, node_key_t v) = 0;
-    virtual void ~deleteEdge(node_key_t u, node_key_t v) = 0;
-    virtual void ~connected(node_key_t u, node_key_t v) const = 0;
+    virtual void insertEdge(node_key_t u, node_key_t v) = 0;
+    virtual void deleteEdge(node_key_t u, node_key_t v) = 0;
+    virtual bool connected(node_key_t u, node_key_t v) const = 0;
 
-    virtual bool containNode(node_key_t key) const = 0;
+    virtual bool containsNode(node_key_t key) const = 0;
     virtual uint64_t getNumNodes() const = 0;
 
     virtual std::string getName() const = 0;
